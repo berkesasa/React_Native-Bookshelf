8311719f30016d86ed9f8fcb4aaf8532d0ff7421
berkesasa <berkesasa11@gmail.com>
2024-06-06 19:34:24 +0300

    configured tailwindcss

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { Text, View, Image, ImageBackground } from 'react-native';
 import { Loading, CustomTextInput, CustomButton } from '../components';
 import { useDispatch, useSelector } from 'react-redux';
 import { setEmail, setPassword, setIsLoading, setLogin, login, autoLogin } from '../redux/userSlice';
 import { useState, useEffect } from 'react';
+import { BlurView } from 'expo-blur';
+
 
 export default function LoginPage({ navigation }) {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const {isLoading, error} = useSelector((state) => state.user)
+  const { isLoading, error } = useSelector((state) => state.user)
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -18,49 +20,57 @@ export default function LoginPage({ navigation }) {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <View className="w-full h-full">
 
       <StatusBar style="auto" />
 
-      <Text style={styles.welcome}>Welcome</Text>
-      <Image
-        style={styles.image}
-        source={require('../../assets/images/loginIcon.png')}
-      />
-      
-      <CustomTextInput
-        title="Email"
-        isSecureText={false}
-        handleOnChangeText={(text) => setEmail(text)}
-        handleValue={email}
-        handlePlaceholder="Enter Your Email"
-      />
+      <ImageBackground source={require('../../assets/background.jpg')} className="w-full h-full flex-1 items-center justify-center">
 
-      <CustomTextInput
-        title="Password"
-        isSecureText={true}
-        handleOnChangeText={(password) => setPassword(password)}
-        handleValue={password}
-        handlePlaceholder="Enter Your Password"
-      />
+        <View className="items-center justify-center rounded-xl overflow-hidden relative w-full">
 
-      {error && <Text>Kullanıcı adı ve ya şifre hatalı</Text>}
+          <Text className="text-[30px] font-bold mb-5 text-white">Welcome</Text>
+          <Image
+            className="w-32 h-32 mb-5"
+            source={require('../../assets/images/loginIcon.png')}
+          />
 
-      <CustomButton
-        buttonText="Login"
-        setWidth="80%"
-        handleOnPress={() => dispatch(login({email, password}))}
-        buttonColor="blue"
-        pressButtonColor="lightblue"
-      />
+          <CustomTextInput
+            title="Email"
+            isSecureText={false}
+            handleOnChangeText={(text) => setEmail(text)}
+            handleValue={email}
+            handlePlaceholder="Enter Your Email"
+          />
 
-      <CustomButton
-        buttonText="Sign Up"
-        setWidth="30%"
-        handleOnPress={() => navigation.navigate('Signup')}
-        buttonColor="red"
-        pressButtonColor="pink"
-      />
+          <CustomTextInput
+            title="Password"
+            isSecureText={true}
+            handleOnChangeText={(password) => setPassword(password)}
+            handleValue={password}
+            handlePlaceholder="Enter Your Password"
+          />
+
+          {error && <Text>Kullanıcı adı ve ya şifre hatalı</Text>}
+
+          <CustomButton
+            buttonText="Login"
+            setWidth="80%"
+            handleOnPress={() => dispatch(login({ email, password }))}
+            buttonColor="blue"
+            pressButtonColor="lightblue"
+          />
+
+          <CustomButton
+            buttonText="Sign Up"
+            setWidth="30%"
+            handleOnPress={() => navigation.navigate('Signup')}
+            buttonColor="red"
+            pressButtonColor="pink"
+          />
+        </View>
+      </ImageBackground>
+
+
 
       {isLoading
         ? <Loading name="Login" changeIsLoading={() => dispatch(setIsLoading(false))} />
@@ -68,36 +78,3 @@ export default function LoginPage({ navigation }) {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-    width: '100%',
-    flex: 1,
-    backgroundColor: 'tomato',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputContainer: {
-    width: '80%',
-  },
-
-  signUpButton: {
-    width: '30%',
-    height: 50,
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    width: 100,
-    height: 100,
-    marginBottom: 20,
-  },
-  welcome: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginBottom: 20,
-    color: 'white',
-  },
-});
